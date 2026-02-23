20ea7b94b0a221ad8adbd3de5167ac1b61c753b0
Anjula <wanjulasd@gmail.com>
2026-02-23 18:48:34 +0530

    Add module import and log output from myFunction

--- app.js
+++ app.js
@@ -41,3 +41,9 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 }).on('error', (err) => {
  console.log("Error: " + err.message);
 });
+
+
+
+
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
