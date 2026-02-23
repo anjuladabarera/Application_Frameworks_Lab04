bffc889ab601890055a46e0eb8f38f74e20f7a9f
Anjula <wanjulasd@gmail.com>
2026-02-23 18:12:13 +0530

    Add file reading functionality and create file.txt with sample content

--- app.js
+++ app.js
@@ -2,4 +2,14 @@
 console.log("Hello from Anjula!");
 
 
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+
+
 
