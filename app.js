23af96f9befa51ccc019a8a6d9b5a4338c0f2805
Anjula <wanjulasd@gmail.com>
2026-02-23 18:54:21 +0530

    Implement async function to handle promise resolution in app.js

--- app.js
+++ app.js
@@ -69,3 +69,28 @@ myPromise.then((result) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+async function myFunction() {
+ try {
+ const result = await myPromise;
+ console.log(result);
+ } catch (error) {
+ console.log(error);
+ }
+}
+myFunction();
+
+
+
+
+
